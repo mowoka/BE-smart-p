d238f6ace1c7062542294bc8224e56a3d50f80b5
Giofanny Mowoka <giofanny@softwareseni.com>
2023-04-01 00:14:27 +0700

    setup documentation

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,11 @@
+import { Controller } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { AuthService } from './auth.service';
+
+
+@ApiTags('Auth')
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NestJS SMART-P API')
+    .setDescription('End Point for SMART-P API Application')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
